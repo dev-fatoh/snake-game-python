69b901f9402556d5e13e52b4a8d8a20abcadbe7a
dev-fatoh <fatohnasr360@gmail.com>
2025-09-16 15:08:50 +0300

    - Add main.py with a simple turtle graphics example - Add .gitignore to exclude venv directory - Add empty requirements.txt for future dependencies

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmey = Turtle()
+
+my_screen = Screen()
+timmey.shape("turtle")
+timmey.color("green")
+timmey.forward(100)
+
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
