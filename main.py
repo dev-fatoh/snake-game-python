5ffd596f2fde2b1b2c1ab4a72315d108ffca4bdd
dev-fatoh <fatohnasr360@gmail.com>
2025-09-20 14:19:39 +0300

    Implement the initial version of the Snake game using the turtle module. Includes Snake class with movement and direction controls. Removes old turtle race code from main.py and adds new game loop. Adds compiled snake.pyc to __pycache__.

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-import turtle
-import random
-# colors = colorgram.extract("image.jpg", 30)
-# rgb_colors = []
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     rgb_colors.append((r, g, b))
-# print(rgb_colors)
+from turtle import Screen, Turtle
+import time
+from snake import Snake
 
-screen = turtle.Screen()
-screen.setup(width=500, height=400)
-is_race_on = False
-user_bet = turtle.textinput(
-    title="Make your bet", prompt="Which tutle will win the race? Enter a color : "
-)
-colors = ["red", "green", "yellow", "blue", "black", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
-all_turtles = []
-for turtle_index in range(0, 6):
-    new_turtle = turtle.Turtle(shape="turtle")
-    new_turtle.color(colors[turtle_index])
-    new_turtle.penup()
-    new_turtle.goto(x=-230, y=y_positions[turtle_index])
-    all_turtles.append(new_turtle)
-if user_bet:
-    is_race_on = True
-while is_race_on:
-    for turtle_racer in all_turtles:
-        if turtle_racer.xcor() > 230:
-            is_race_on = False
-            winning_color = turtle_racer.pencolor()
-            if winning_color == user_bet:
-                print(f"You have Won! The {winning_color} turtle is the winner !!!!")
-            else:
-                print(f"You have Lost! The {winning_color} turtle is the winner !!!!")
-        random_distance = random.randint(0, 10)
-        turtle_racer.forward(random_distance)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake game")
+screen.tracer(0)
+game_is_on = True
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
 screen.exitonclick()
